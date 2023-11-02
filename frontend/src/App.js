9d7e94316d7621dd6faf94c5fe029bad73fcd059
Akash Dhamane <akashdhamane560@gmail.com>
2023-11-02 12:52:04 +0530

    complaint basic added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,8 @@ import Alert from './components/Alert';
 import Admission from './components/Admission';
 import Studentdetails from './components/Studentdetails';
 import Facilities from './components/Facilities';
+import Complaint from './components/Complaint';
+import SendComplaint from './components/SendComplaint';
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
@@ -39,6 +41,8 @@ function App() {
       <Route exact path='/signup' element={<Signup isDarkTheme={isDarkTheme} showAlert={showAlert}/>} />
       <Route exact path="/map" element= {<Map  isDarkTheme={isDarkTheme} showAlert={showAlert}/>}/>
       <Route exact path="/" element= {<Sliding  isDarkTheme={isDarkTheme} />}/>
+      <Route exact path="/complaint" element= {<Complaint  isDarkTheme={isDarkTheme} />}/>
+      <Route exact path="/sendcomplaint" element= {<SendComplaint  isDarkTheme={isDarkTheme} />}/>
       <Route exact path="/facilities" element= {<Facilities  isDarkTheme={isDarkTheme} />}/>
       <Route exact path="/admission" element = {<Admission isDarkTheme={isDarkTheme}/>} />
       <Route exact path="/studentNotice" element= {<StudentNotice notices={notices} isDarkTheme={isDarkTheme} 
