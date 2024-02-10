2f40671b20b82cc3e5f53126693eef911549dcaf
Akash Dhamane <akashdhamane560@gmail.com>
2024-02-10 15:52:08 +0530

    minute changes

--- frontend/src/components/Admission_recieved.js
+++ frontend/src/components/Admission_recieved.js
@@ -20,7 +20,14 @@ const Admission_recieved = () => {
                         <th scope="col">#</th>
                         <th scope="col">Registration No.</th>
                         <th scope="col">Name</th>
-                        <th scope="col">Contact No.</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">D.O.B</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Mobile No.</th>
+                        <th scope="col">Year</th>
+                        <th scope="col">Branch</th>
+                        <th scope="col">Home Address</th>
+                        <th scope="col">Block</th>
                         {/* <th scope="col">Delete</th>
                         <th scope="col">Update</th> */}
                     </tr>
@@ -31,7 +38,15 @@ const Admission_recieved = () => {
                             <th scope="row">{index + 1}</th>
                             <td>{detail.regId}</td>
                             <td>{detail.fullName}</td>
+                            <td>{detail.email}</td>
+                            <td>{detail.dateOfBirth}</td>
+                            <td>{detail.gender}</td>
                             <td>{detail.mobileNumber}</td>
+                           
+                            <td>{detail.year}</td>
+                            <td>{detail.branch}</td>
+                            <td>{detail.homeAddress}</td>
+                            <td>{detail.block}</td>
                             {/* <td><i className="fa-solid fa-trash mx-2" onClick={() => handleDelete(detail._id)}></i></td>
                             <td><i className="fa-regular fa-pen-to-square mx-2" onClick={() => handleUpdate1()}></i></td> */}
                         </tr>
